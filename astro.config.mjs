7c52450fd715fabde7db7d113dc3afb1545bf6a5
Sufyan Liaquat <sufyanliaquat58@gmail.com>
2026-01-27 02:06:49 +0500

    added initial portfolio structure, layout, and core UI components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sufyanliaqat.me',
   integrations: [react(), sitemap()],
 
   vite: {
